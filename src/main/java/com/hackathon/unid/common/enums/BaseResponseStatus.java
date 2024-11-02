85d6079e3ede47a43211998e004d939becfd3eab
joonghyun <joongh@ewhain.net>
2024-11-02 15:09:33 +0900

    feature: upload BaseResponseStatus

---
+++ src/main/java/com/hackathon/unid/common/enums/BaseResponseStatus.java
@@ -0,0 +1,36 @@
+package com.hackathon.unid.common.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum BaseResponseStatus {
+    /**
+     * 1000: 요청 성공
+     */
+    SUCCESS(true, 1000, "요청에 성공하였습니다."),
+
+    /**
+     * 2000: Request 오류
+     */
+    // users(2000-2099)
+
+    /**
+     * 3000: Response 오류
+     */
+    // users(3000~3099)
+
+    /**
+     * 4000: DB, Server 오류
+     */
+    DATABASE_ERROR(false, 4000, "데이터베이스 연결에 실패했습니다.");
+
+    private final boolean isSuccess;
+    private final int code;
+    private final String message;
+
+    BaseResponseStatus(boolean isSuccess, int code, String message) {
+        this.isSuccess = isSuccess;
+        this.code = code;
+        this.message = message;
+    }
+}
